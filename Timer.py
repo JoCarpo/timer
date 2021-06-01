57a181b24088240cb8292eeac1c3872ea7468078
JoCarpo <81490409+JoCarpo@users.noreply.github.com>
2021-06-02 09:57:38 +1000

    Update Timer.py

--- Timer.py
+++ Timer.py
@@ -6,7 +6,6 @@ m = int(input("How many minutes: "))
 h = int(input("How many hours: "))
 
 sys.stdout.write(f"\r{h}:{m}:{s}")
-sys.stdout.flush()
 time.sleep(1)
 sys.stdout.write("\r            ")
 
@@ -17,14 +16,12 @@ while True:
             sys.stdout.write(f"\r{h}:{m}:{s}")
             time.sleep(1)
             sys.stdout.write("\r            ")
-            sys.stdout.flush()
         elif m > 0:
             m -= 1
             s = 59
             sys.stdout.write(f"\r{h}:{m}:{s}")
             time.sleep(1)
             sys.stdout.write("\r            ")
-            sys.stdout.flush()
         else:
             h -= 1
             m = 59
@@ -32,7 +29,6 @@ while True:
             sys.stdout.write(f"\r{h}:{m}:{s}")
             time.sleep(1)
             sys.stdout.write("\r            ")
-            sys.stdout.flush()
     else:
         print("\rFinished")
         beepy.beep(6)
